4dfcdb54c7946cbf38ea2c8c8c6b701aacf1faa6
Samuel Chen <sche842@gmail.com>
2026-06-21 14:19:18 +1000

    code auto format

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,21 @@ from api.v1.users import api as users_ns
 from config import config
 
 
-def create_app(config_name='default'):
+def create_app(config_name="default"):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    api = Api(
+        app,
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API",
+        doc="/api/v1/",
+    )
 
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(users_ns, path="/api/v1/users")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    api.add_namespace(places_ns, path="/api/v1/places")
+    api.add_namespace(reviews_ns, path="/api/v1/reviews")
 
     return app
