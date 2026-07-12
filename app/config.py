96368f384ff6a17e0f31ad778209ee5c25f14135
bz888 <ziyu.ben158@gmail.com>
2026-07-13 01:20:43 +1000

    add docker files

--- app/config.py
+++ app/config.py
@@ -19,12 +19,9 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(
-            basedir,
-            "..",
-            "development.db"
-        )
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(basedir, "..", "development.db")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
