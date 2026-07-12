40b3b78a81c216c7ae58c0a90284980cb9f9ca78
Samuel Chen <sche842@gmail.com>
2026-07-12 19:28:37 +1000

    absolute path for db in config

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', SECRET_KEY)
@@ -15,8 +19,21 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + os.path.join(
+            basedir,
+            "..",
+            "development.db"
+        )
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+# class DevelopmentConfig(Config):
+#     DEBUG = True
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
